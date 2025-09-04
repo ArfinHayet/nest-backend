6fe7390afbf5808d6ae595107c0cb7bd38458268
Syed Arfin Hayet <45680100+ArfinHayet@users.noreply.github.com>
2025-09-04 12:28:23 +0600

    added empty space

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return `
     <!DOCTYPE html>
     <html lang="en">
-      <head>
+      <head>   
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>API Status</title>
