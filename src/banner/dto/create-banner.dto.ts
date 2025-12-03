135804d3e516cfbbfd6a9507425c06ec9a6a3bd3
Syed Arfin Hayet <45680100+ArfinHayet@users.noreply.github.com>
2025-12-04 01:46:06 +0600

    webhook check

--- src/banner/dto/create-banner.dto.ts
+++ src/banner/dto/create-banner.dto.ts
@@ -4,7 +4,7 @@ export class CreateBannerDto {
   @ApiProperty({ example: '/uploads/banner1.jpg' })
   imagePath: string;
 
-  @ApiProperty({ example: 'New Year Discount' })
+  @ApiProperty({ example: 'New Year Discount' })    
   name: string;
 
   @ApiProperty({ example: 'Special discount banner for New Year', required: false })
